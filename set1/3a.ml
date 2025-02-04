56a86d44a1c927644be6ff29c6e706938b5da6f4
Frank Yin <zi.h.yin@mail.mcgill.ca>
2025-02-04 16:52:58 -0500

    God bless me on 2025-02-04 at 16:52:58

--- set1/3a.ml
+++ set1/3a.ml
@@ -7,11 +7,4 @@ let _ = lookup "x" [("x", 2); ("y", 3)]   (* Some 2 *)
 let _ = lookup "z" [("x", 2)]             (* None *)
 let _ = lookup "x" [("x", 1); ("x", 2)]   (* Some 1 *)
 
-let example1 = match lookup 2 [(1, "a"); (2, "b"); (3, "c")] with
-  | Some v -> "Found: " ^ v
-  | None -> "Not found"
-
-let example2 = match lookup 4 [(1, "a"); (2, "b"); (3, "c")] with
-  | Some v -> "Found: " ^ v
-  | None -> "Not found
 
